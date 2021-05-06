57b06c7c09ab580fcdc28fe9be06ac9e1d1c04b0
JMcCoding <jmccurley@ymail.com>
2021-05-06 17:59:16 -0400

    Login component created

--- vueauth-client/src/router/index.js
+++ vueauth-client/src/router/index.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from '@/components/Login'
-import Dashboard from '@/components/Dashboard'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from "@/components/Login";
+import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router)
 
@@ -9,13 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Login',
-      component: Login
+      name: 'HelloWorld',
+      component: HelloWorld
     },
     {
-      path: '/dashboard',
-      name: 'Dashboard',
-      component: Dashboard
+      path: "/login",
+      name: "Login",
+      component: Login
     }
   ]
 })
