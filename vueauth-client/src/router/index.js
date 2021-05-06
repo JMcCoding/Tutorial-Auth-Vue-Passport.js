aecc186d3539a56a36e4794ee818327f162c6171
Joseph McCurley <jmccu45@wgu.edu>
2021-05-06 18:08:47 -0400

    login = new default

--- vueauth-client/src/router/index.js
+++ vueauth-client/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/login',
       name: 'Login',
       component: Login
     }
