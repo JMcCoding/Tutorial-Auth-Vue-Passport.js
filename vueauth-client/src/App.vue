7640579e057ec0f55271ae837db2f0223068d3e3
JMcCoding <26391897+JMcCoding@users.noreply.github.com>
2021-05-06 23:26:19 -0400

    data is no longer static + app now uses typed in fields to send requests

--- vueauth-client/src/App.vue
+++ vueauth-client/src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div>
+      <router-link :to="{ name: 'Dashboard' }">Dashboard</router-link>
+      <router-link :to="{ name: 'Login' }">Login</router-link>
+      <a href="#" @click="logout">Logout</a>
     <router-view/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import router from '@/router'
+
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    logout: e => {
+      axios.get('/api/logout')
+        .then(() => {
+          router.push('/')
+        })
+    }
+  }
 }
 </script>
 
