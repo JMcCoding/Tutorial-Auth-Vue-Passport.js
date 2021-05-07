6a2a69f42bab055bbc19701724e5f33a501f398d
JMcCoding <26391897+JMcCoding@users.noreply.github.com>
2021-05-06 22:29:58 -0400

    Added login, logout, & get user routes

--- vueauth-server/index.js
+++ vueauth-server/index.js
@@ -22,3 +22,53 @@ app.use(passport.initialize())
 
 app.use(passport.session())
 
+// Create auth check routing middleware
+const authMiddleware = (req, res, next) => {
+  if (!req.isAuthenticated())
+    res.status(401).send('You are not authenticated')
+  else
+    return next()
+}
+
+// Simulated Database Information (just an array)
+let users = [
+  {
+    id: 1,
+    name: 'Jude',
+    email: 'jude@email.com',
+    password: 'password'
+  },
+  {
+    id: 2,
+    name: 'Emma',
+    email: 'emma@email.com',
+    password: 'password2'
+  }
+]
+
+// Set up server routes
+app.post('/api/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err)
+    if (!user)
+      return res.status(400).send([user, 'Cannot log in', info])
+    req.login(user, err => res.send('Logged in'))
+  })(req, res, next)
+})
+
+app.get('/api/logout', (req, res) => {
+  req.logout()
+
+  console.log('Logged out')
+
+  return res.send()
+})
+
+app.get('/api/user', authMiddleware, (req, res) => {
+  let user = users.find(user => user.id === req.session.passport.user)
+  
+  console.log([user, req.session])
+
+  res.send({user: user})
+})
