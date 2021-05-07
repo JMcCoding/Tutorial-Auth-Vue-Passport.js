0f740e6b47ed42d8259b819332f1bef7e90976e6
JMcCoding <26391897+JMcCoding@users.noreply.github.com>
2021-05-06 23:50:46 -0400

    Final proxie removal

--- vueauth-client/config/index.js
+++ vueauth-client/config/index.js
@@ -11,7 +11,6 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api': 'http://localhost:3000'
       // '/api': 'http://localhost:3000'
     },
 
