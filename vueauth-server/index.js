41bf8dfca0df3e8845df524192fef9bb227fc898
JMcCoding <26391897+JMcCoding@users.noreply.github.com>
2021-05-06 22:18:22 -0400

    Initializing middleware for server

--- vueauth-server/index.js
+++ vueauth-server/index.js
@@ -8,3 +8,17 @@ const passport = require('passport')
 
 // Get the local passport authentication Strategy
 const LocalStrategy = require('passport-local').Strategy
+
+// Initialize middleware
+app.use(bodyParser.json()) // May no longer be necessary.
+
+app.use(cookieSession({
+  name: 'randomSessionName',
+  keys: ['vueauthrandomkey'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+app.use(passport.initialize())
+
+app.use(passport.session())
+
